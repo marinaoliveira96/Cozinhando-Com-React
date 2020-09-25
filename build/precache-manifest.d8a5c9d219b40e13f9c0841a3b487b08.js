463aea7f9a739070c5e16e4ef18c9e09d02653a6
Marina Oliveira <marina09oliveira@gmail.com>
2020-09-25 17:30:04 -0300

    first commit

---
+++ build/precache-manifest.d8a5c9d219b40e13f9c0841a3b487b08.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "967ac00ed01610236e6fe47f0e5062b4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d7a2aad022fde25d1e7d",
+    "url": "/static/js/2.e6a79adb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e6a79adb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9bb5bff8b0bb077a31d6",
+    "url": "/static/js/main.527b7e7d.chunk.js"
+  },
+  {
+    "revision": "463b0e5bfd227d7bd498",
+    "url": "/static/js/runtime-main.5c9ed490.js"
+  }
+]);
---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import Counter from './Counter';
+import CounterHooks from './CounterHooks';
+
+export const ThemeContext = React.createContext();
+function App() {
+  const [theme, setTheme] = useState('red');
+  return (
+    <ThemeContext.Provider value={{ backgroundColor: theme }}>
+      Counter
+      <Counter initialCount={0} />
+      CounterHooks
+      <CounterHooks initialCount={5} />
+      <button
+        onClick={() =>
+          setTheme((prev) => {
+            return prev === 'red' ? 'blue' : 'red';
+          })
+        }
+      >
+        Toggle Theme
+      </button>
+    </ThemeContext.Provider>
+  );
+}
+
+export default App;
