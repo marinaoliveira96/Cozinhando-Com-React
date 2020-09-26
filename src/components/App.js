317ed9393c2c911eacd7e2c074abe46dd0c813af
Marina Oliveira <marina09oliveira@gmail.com>
2020-09-26 15:20:33 -0300

    useContext + useEffect

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,50 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import RecipeList from './RecipeList';
+import { uuid } from 'uuidv4';
 import '../css/app.css';
 
+export const RecipeContext = React.createContext();
+const LOCAL_STORAGE_KEY = 'cookingWithReact.recipes';
+
 function App() {
-  return <RecipeList recipes={sampleRecipes} />;
+  const [recipes, setRecipes] = useState(sampleRecipes);
+
+  useEffect(() => {
+    const recipeJSON = localStorage.getItem(LOCAL_STORAGE_KEY);
+    if (recipeJSON != null) setRecipes(JSON.parse(recipeJSON));
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(recipes));
+  }, [recipes]);
+
+  const recipeContextValue = {
+    handleRecipeAdd,
+    handleRecipeDelete,
+  };
+
+  function handleRecipeAdd() {
+    const newRecipe = {
+      id: uuid(),
+      name: 'New',
+      servings: 1,
+      cookTime: '1:00',
+      instructions: 'Instr.',
+      ingredients: [{ id: uuid(), name: 'Name', amount: '1 Tbs' }],
+    };
+
+    setRecipes([...recipes, newRecipe]);
+  }
+
+  function handleRecipeDelete(id) {
+    setRecipes(recipes.filter((recipe) => recipe.id !== id));
+  }
+
+  return (
+    <RecipeContext.Provider value={recipeContextValue}>
+      <RecipeList recipes={recipes} />
+    </RecipeContext.Provider>
+  );
 }
 
 const sampleRecipes = [
--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import IngredientList from './IngredientList';
+import { RecipeContext } from './App';
 
 export default function Recipe(props) {
-  const { name, cookTime, servings, instructions, ingredients } = props;
+  const { handleRecipeDelete } = useContext(RecipeContext);
+  const { name, id, cookTime, servings, instructions, ingredients } = props;
+
   return (
     <div className="recipe">
       <div className="recipe__header">
         <h3 className="recipe__title">{name}</h3>
         <div>
           <button className="btn btn--primary mr-1">Edit</button>
-          <button className="btn btn--danger">Delete</button>
+          <button
+            className="btn btn--danger"
+            onClick={() => handleRecipeDelete(id)}
+          >
+            Delete
+          </button>
         </div>
       </div>
       <div className="recipe__row">
